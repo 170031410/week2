1d98de28fb5362f12f7acfa1840df6f002e00ad2
170031410@kluniversity.omgit config --global  user.name 170031410@kluniversity.in <170031410@kluniversity.in>
2020-03-09 19:10:32 +0530

    week2

---
+++ src/week2/App1.java
@@ -0,0 +1,10 @@
+package week2;
+
+public class App1 {
+	
+	    public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+	}
+
---
+++ src/week2/NoOfChocolates.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class NoOfChocolates implements MainInterface1{
+	
+		int i;
+		public int chocolate_sweets(int chocolates[],int sweets[],int total_weight) {
+			for(i=0;i<chocolates.length;i++) {
+				total_weight=total_weight-chocolates[i];
+			}
+			for(i=0;i<sweets.length;i++) {
+				total_weight=total_weight-sweets[i];
+			}
+			return total_weight;
+		}
+		public int no_of_candy(int low,int high,int x) {
+			if(x>=low&&x<=high) {
+				return 1;
+			}
+			else
+				return 0;
+		}
+	}
+
